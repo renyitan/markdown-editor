f1dc7a35f638ace69f169f0c2c87e2f697b7ce67
Renyi Tan <tanrenyi@gmail.com>
2022-06-20 11:33:09 +0800

    wrap editable in fluid-ui textarea

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
-import { createEditor, BaseEditor, Descendant } from 'slate';
-import { Slate, Editable, withReact, ReactEditor } from 'slate-react';
+import React, { useState, useCallback } from 'react';
+import {
+  createEditor,
+  BaseEditor,
+  Descendant,
+  Transforms,
+  Editor,
+} from 'slate';
+import {
+  Slate,
+  Editable,
+  withReact,
+  ReactEditor,
+  DefaultEditable,
+} from 'slate-react';
+import { Button, TextArea, Box } from '@fluentui/react-northstar';
 
-type CustomElement = { type: 'paragraph'; children: CustomText[] };
+type CustomElement = { type: any; children: CustomText[] };
 type CustomText = { text: string };
 
 declare module 'slate' {
@@ -13,30 +26,70 @@ declare module 'slate' {
   }
 }
 
-const initialValue: Descendant[] = [
+const initialValue = [
   {
     type: 'paragraph',
     children: [{ text: 'A line of text in a paragraph.' }],
   },
 ];
 
-function App() {
+const App = () => {
   const [editor] = useState(() => withReact(createEditor()));
 
-  return (
-    <Slate editor={editor} value={initialValue}>
-      <Editable
-        onKeyDown={(event) => {
+  // Define a rendering function based on the element passed to `props`. We use
+  // `useCallback` here to memoize the function for subsequent renders.
+  const renderElement = useCallback((props: any) => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
+  const CustomTextArea = () => {
+    return (
+      <TextArea
+        as={Editable}
+        renderElement={renderElement}
+        onKeyDown={(event: any) => {
           if (event.key === '&') {
-            // Prevent the ampersand character from being inserted.
             event.preventDefault();
-            // Execute the `insertText` method when the event occurs.
-            editor.insertText('and');
+            // Determine whether any of the currently selected blocks are code blocks.
+            const [match]: any = Editor.nodes(editor, {
+              match: (n: any) => n.type === 'code',
+            });
+            // Toggle the block type depending on whether there's already a match.
+            Transforms.setNodes(
+              editor,
+              { type: match ? 'paragraph' : 'code' },
+              { match: (n) => Editor.isBlock(editor, n) }
+            );
           }
         }}
       />
-    </Slate>
+    );
+  };
+
+  return (
+    <>
+      <Slate editor={editor} value={initialValue}>
+        <CustomTextArea />
+      </Slate>
+    </>
   );
-}
+};
+
+const CodeElement = (props: any) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = (props: any) => {
+  return <p {...props.attributes}>{props.children}</p>;
+};
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider, teamsTheme } from '@fluentui/react-northstar';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider theme={teamsTheme}>
     <App />
-  </React.StrictMode>
+  </Provider>
+  // </React.StrictMode>
 );
